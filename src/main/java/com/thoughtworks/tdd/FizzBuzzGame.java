226480ff0675bdb1e256a3838690d8a2c3341fd5
Ruifeng-Wu <296078293@qq.com>
2019-04-18 17:14:23 +0800

    feat:implement FizzBuzzGame return Fizz

--- src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
@@ -7,6 +7,9 @@ package com.thoughtworks.tdd;
  */
 public class FizzBuzzGame {
     public String fizzBuzz(int number) {
+        if (number%3==0){
+            return "Fizz";
+        }
         return ""+number;
     }
 }
