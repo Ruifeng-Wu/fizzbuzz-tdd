eeb9091edbaa407d4555527e1829eaa90a00b99e
Ruifeng-Wu <296078293@qq.com>
2019-04-18 17:15:16 +0800

    test:FizzBuzzGame return Buzz

--- src/test/java/com/thoughtworks/tdd/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzGameTest.java
@@ -11,14 +11,20 @@ import static org.junit.Assert.assertThat;
  * @date 2019/4/17 17:12
  */
 public class FizzBuzzGameTest {
-    FizzBuzzGame fizzBuzzGame=new FizzBuzzGame();
+    FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
     @Test
     public void returns_the_number_given_number_not_divisible_by_3_5_nor_7() throws Exception {
-        assertThat(fizzBuzzGame.fizzBuzz(1),is("1"));
+        assertThat(fizzBuzzGame.fizzBuzz(1), is("1"));
     }
 
     @Test
     public void returns_Fizz_given_number_divisible_by_3() throws Exception {
-        assertThat(fizzBuzzGame.fizzBuzz(3),is("Fizz"));
+        assertThat(fizzBuzzGame.fizzBuzz(3), is("Fizz"));
+    }
+
+    @Test
+    public void returns_Buzz_given_number_divisible_by_5() throws Exception {
+        assertThat(fizzBuzzGame.fizzBuzz(5), is("Buzz"));
     }
 }
