d82ceb7a85d97553be32d8dcef8c4a678d91b5a1
Ruifeng-Wu <296078293@qq.com>
2019-04-20 13:59:08 +0800

    Update FizzBuzzGame.java

--- src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
@@ -6,29 +6,27 @@ package com.thoughtworks.tdd;
  * @date 2019/4/18 17:09
  */
 public class FizzBuzzGame {
+    final static int DIVIDEND3 = 3;
+    final static int DIVIDEND5 = 5;
+    final static int DIVIDEND7 = 7;
+
     public String fizzBuzz(int number) {
         String result = "";
-        if (divisible3(number)) {
+        if (aliquot(number, DIVIDEND3)) {
             result += "Fizz";
         }
-        if (divisible5(number)) {
+        if (aliquot(number, DIVIDEND5)) {
             result += "Buzz";
         }
-        if (divisible7(number)) {
+        if (aliquot(number, DIVIDEND7)) {
             result += "Whizz";
         }
         return "".equals(result) ? String.valueOf(number) : result;
     }
 
-    private boolean divisible7(int number) {
-        return number % 7 == 0;
+    private boolean aliquot(int number, int dividend) {
+        return number % dividend == 0;
     }
 
-    private boolean divisible5(int number) {
-        return number % 5 == 0;
-    }
 
-    private boolean divisible3(int number) {
-        return number % 3 == 0;
-    }
 }
